87f425690cfcd64e818917182dc3608475fdcb7e
Vllad Gordeev <vlladg@gmail.com>
2023-09-09 17:25:44 +0300

    Add i18next

--- src/001_app/App.tsx
+++ src/001_app/App.tsx
@@ -1,21 +1,32 @@
-import React, { Suspense } from 'react';
+import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import {classNames} from '006_shared/lib/styles';
 import {MainPage} from '002_pages/MainPage';
 import {AboutPage} from '002_pages/AboutPage';
+import {useAppTheme} from '001_app/providers/appThemeProvider/lib/useAppTheme';
+import {useTranslation} from 'react-i18next';
 
 function App() {
+
+    const {appTheme} = useAppTheme()
+    const {t, i18n} = useTranslation()
+
+    const toggleLang = () => {
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
+    }
+
     return (
-        <div className={classNames('app')}>
-            <Link to="/">Главная страница</Link>
-            <Link to="/about">О компании</Link>
-            <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className={classNames('app', {}, [appTheme])}>
+                <Link to="/">{t('Главная страница')}</Link>
+                <Link to="/about">{t('О компании')}</Link>
                 <Routes>
                     <Route path="/" element={<MainPage />} />
                     <Route path="/about" element={<AboutPage />} />
                 </Routes>
-            </Suspense>
-        </div>
+                <button onClick={toggleLang}>{t('Перевод')}</button>
+            </div>
+        </Suspense>
     );
 }
 
