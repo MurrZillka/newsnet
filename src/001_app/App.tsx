acf58ffcd842b4edec10a8bd468c705119d04fc5
Vllad Gordeev <vlladg@gmail.com>
2023-09-09 13:41:57 +0300

    Add scss files in appThemes
    * dark.scss
    * normal.scss
    * index.scss

--- src/001_app/App.tsx
+++ src/001_app/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import {MainPage} from '002_pages/MainPage';
 import {AboutPage} from '002_pages/AboutPage/ui/AboutPage';
+import {classNames} from '006_shared/lib/styles';
 
 function App() {
   return (
-    <div className="app">
+    <div className={classNames('app')}>
         <Link to="/">Главная страница</Link>
         <Link to="/about">О компании</Link>
 
